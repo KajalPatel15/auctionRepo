355ff1d96da87cefad50759a52f6498a8764ea0c
KajalPatel15 <106984568+KajalPatel15@users.noreply.github.com>
2022-06-08 01:01:59 -0400

    Update AuctionController.java

--- src/main/java/com/auction/auctionservice/controller/AuctionController.java
+++ src/main/java/com/auction/auctionservice/controller/AuctionController.java
@@ -1,23 +1,34 @@
+
 package com.auction.auctionservice.controller;
 
 import com.auction.auctionservice.domain.reponse.AuctionResponse;
 import com.auction.auctionservice.domain.request.AuctionRequest;
+import com.auction.auctionservice.model.Item;
+import com.auction.auctionservice.repository.ItemJpaRepository;
 import com.auction.auctionservice.service.AuctionService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController("/auction")
 @AllArgsConstructor
 public class AuctionController {
 
     AuctionService auctionService;
+    ItemJpaRepository itemJpaRepository;
 
     @PostMapping("/auction-items")
     public AuctionResponse reserveAuction(@Valid @RequestBody AuctionRequest auctionRequest){
         return auctionService.processAuction(auctionRequest);
+    }
+
+    @GetMapping("/products")
+    public List<Item> getAllProducts(){
+        return itemJpaRepository.findAll();
     }
 }
